1040d7e3ff4909891331c92cb696af291569f02d
hankelc <taichen.liu@imorpheus.io>
2021-07-15 23:10:30 +0800

    add libTAU namespace in new msg alert

--- include/libTAU/alert_types.hpp
+++ include/libTAU/alert_types.hpp
@@ -1427,7 +1427,7 @@ TORRENT_VERSION_NAMESPACE_3
     struct TORRENT_EXPORT communication_new_message_alert final : alert
     {
         // internal
-        TORRENT_UNEXPORT communication_new_message_alert(aux::stack_allocator& alloc, communication::message t);
+        TORRENT_UNEXPORT communication_new_message_alert(aux::stack_allocator& alloc, libTAU::communication::message t);
 
         TORRENT_DEFINE_ALERT_PRIO(communication_new_message_alert, 38, alert_priority::critical)
 
@@ -1436,7 +1436,7 @@ TORRENT_VERSION_NAMESPACE_3
         std::string message() const override;
 
         // message found from peers.
-        communication::message msg;
+        libTAU::communication::message msg;
     };
 
     // this alert is posted when message hash is confirmed by peers.
