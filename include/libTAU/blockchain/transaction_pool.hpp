9d5762b28b364d6612d5ca22b69fdbc2a787356b
cuitaixiang <754060645@qq.com>
2021-09-22 22:13:43 +0800

    add pool

---
+++ include/libTAU/blockchain/transaction_pool.hpp
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2021, TaiXiang Cui
+All rights reserved.
+
+You may use, distribute and modify this code under the terms of the BSD license,
+see LICENSE file.
+*/
+
+#ifndef LIBTAU_TRANSACTION_POOL_HPP
+#define LIBTAU_TRANSACTION_POOL_HPP
+
+
+namespace libTAU::blockchain {
+    class transaction_pool {
+
+    };
+}
+
+
+#endif //LIBTAU_TRANSACTION_POOL_HPP
