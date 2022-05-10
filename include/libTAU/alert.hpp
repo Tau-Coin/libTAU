a6f5fe8a6ce75c2af53937f888e2c5e6541182c7
hankelc <taichenliu@imorpheus.io>
2022-05-10 11:37:41 +0800

    modifications in alert, delete useless alert in libTAU

--- include/libTAU/alert.hpp
+++ include/libTAU/alert.hpp
@@ -73,56 +73,17 @@ namespace alert_category {
 	// Enables alerts for port mapping events. For NAT-PMP and UPnP.
 	inline constexpr alert_category_t port_mapping = 2_bit;
 
-	// Enables alerts for events related to the storage. File errors and
-	// synchronization events for moving the storage, renaming files etc.
-	inline constexpr alert_category_t storage = 3_bit;
-
-	// Enables all tracker events. Includes announcing to trackers,
-	// receiving responses, warnings and errors.
-	inline constexpr alert_category_t tracker = 4_bit;
-
-	// Low level alerts for when peers are connected and disconnected.
-	inline constexpr alert_category_t connect = 5_bit;
-
-		// Enables alerts for when a torrent or the session changes state.
+	// Enables alerts for when a torrent or the session changes state.
 	inline constexpr alert_category_t status = 6_bit;
-
-	// Alerts when a peer is blocked by the ip blocker or port blocker.
-	inline constexpr alert_category_t ip_block = 7_bit;
-
-	// Alerts when some limit is reached that might limit the download
-	// or upload rate.
-	inline constexpr alert_category_t performance_warning = 8_bit;
 
 	// Alerts on events in the DHT node. For incoming searches or
 	// bootstrapping being done etc.
 	inline constexpr alert_category_t dht = 9_bit;
 
-	// If you enable these alerts, you will receive a stats_alert
-	// approximately once every second, for every active torrent.
-	// These alerts contain all statistics counters for the interval since
-	// the lasts stats alert.
-	inline constexpr alert_category_t stats = 10_bit;
-
 	// Enables debug logging alerts. These are available unless libTAU
 	// was built with logging disabled (``TORRENT_DISABLE_LOGGING``). The
 	// alerts being posted are log_alert and are session wide.
 	inline constexpr alert_category_t session_log = 11_bit;
-
-	// Enables debug logging alerts for torrents. These are available
-	// unless libTAU was built with logging disabled
-	// (``TORRENT_DISABLE_LOGGING``). The alerts being posted are
-	// torrent_log_alert and are torrent wide debug events.
-	inline constexpr alert_category_t torrent_log = 12_bit;
-
-	// Enables debug logging alerts for peers. These are available unless
-	// libTAU was built with logging disabled
-	// (``TORRENT_DISABLE_LOGGING``). The alerts being posted are
-	// peer_log_alert and low-level peer events and messages.
-	inline constexpr alert_category_t peer_log = 13_bit;
-
-	// enables the incoming_request_alert.
-	inline constexpr alert_category_t incoming_request = 14_bit;
 
 	// enables dht_log_alert, debug logging for the DHT
 	inline constexpr alert_category_t dht_log = 15_bit;
@@ -134,33 +95,16 @@ namespace alert_category {
 	// for debugging the UPnP or NAT-PMP implementation
 	inline constexpr alert_category_t port_mapping_log = 17_bit;
 
-	// enables verbose logging from the piece picker.
-	inline constexpr alert_category_t picker_log = 18_bit;
-
-	// alerts when files complete downloading
-	inline constexpr alert_category_t file_progress = 19_bit;
-
-	// alerts when pieces complete downloading or fail hash check
-	inline constexpr alert_category_t piece_progress = 20_bit;
-
-	// alerts when we upload blocks to other peers
-	inline constexpr alert_category_t upload = 21_bit;
-
-	// alerts on individual blocks being requested, downloading, finished,
-	// rejected, time-out and cancelled. This is likely to post alerts at a
-	// high rate.
-	inline constexpr alert_category_t block_progress = 22_bit;
-
 	// alerts on events in communication
     inline constexpr alert_category_t communication = 23_bit;
 
     // alerts on communication log
     inline constexpr alert_category_t communication_log = 24_bit;
 
-    // alerts on events in communication
+    // alerts on events in blockchain
     inline constexpr alert_category_t blockchain = 25_bit;
 
-    // alerts on communication log
+    // alerts on blockchain log
     inline constexpr alert_category_t blockchain_log = 26_bit;
 
 	// The full bitmask, representing all available categories.
@@ -188,51 +132,19 @@ namespace alert_category {
 		alert& operator=(alert const&) = delete;
 		alert(alert&& rhs) noexcept = default;
 
-#if TORRENT_ABI_VERSION == 1
-		using category_t = alert_category_t;
-#endif
-
 		static inline constexpr alert_category_t error_notification = 0_bit;
 		static inline constexpr alert_category_t peer_notification = 1_bit;
 		static inline constexpr alert_category_t port_mapping_notification = 2_bit;
-		static inline constexpr alert_category_t storage_notification = 3_bit;
-		static inline constexpr alert_category_t tracker_notification = 4_bit;
-		static inline constexpr alert_category_t connect_notification = 5_bit;
-#if TORRENT_ABI_VERSION == 1
-		TORRENT_DEPRECATED
-		static inline constexpr alert_category_t debug_notification = connect_notification;
-#endif
 		static inline constexpr alert_category_t status_notification = 6_bit;
-#if TORRENT_ABI_VERSION == 1
-		// Alerts for when blocks are requested and completed. Also when
-		// pieces are completed.
-		TORRENT_DEPRECATED
-		static inline constexpr alert_category_t progress_notification = 7_bit;
-#endif
-		static inline constexpr alert_category_t ip_block_notification = 8_bit;
-		static inline constexpr alert_category_t performance_warning = 9_bit;
-		static inline constexpr alert_category_t dht_notification = 10_bit;
-
-#if TORRENT_ABI_VERSION <= 2
-		// If you enable these alerts, you will receive a stats_alert
-		// approximately once every second, for every active torrent.
-		// These alerts contain all statistics counters for the interval since
-		// the lasts stats alert.
-		TORRENT_DEPRECATED
-		static inline constexpr alert_category_t stats_notification = 11_bit;
-#endif
-		static inline constexpr alert_category_t session_log_notification = 13_bit;
-		static inline constexpr alert_category_t torrent_log_notification = 14_bit;
-		static inline constexpr alert_category_t peer_log_notification = 15_bit;
-		static inline constexpr alert_category_t incoming_request_notification = 16_bit;
-		static inline constexpr alert_category_t dht_log_notification = 17_bit;
-		static inline constexpr alert_category_t dht_operation_notification = 18_bit;
-		static inline constexpr alert_category_t port_mapping_log_notification = 19_bit;
-		static inline constexpr alert_category_t picker_log_notification = 20_bit;
-		static inline constexpr alert_category_t file_progress_notification = 21_bit;
-		static inline constexpr alert_category_t piece_progress_notification = 22_bit;
-		static inline constexpr alert_category_t upload_notification = 23_bit;
-		static inline constexpr alert_category_t block_progress_notification = 24_bit;
+		static inline constexpr alert_category_t dht_notification = 9_bit;
+		static inline constexpr alert_category_t session_log_notification = 11_bit;
+		static inline constexpr alert_category_t dht_log_notification = 15_bit;
+		static inline constexpr alert_category_t dht_operation_notification = 16_bit;
+		static inline constexpr alert_category_t port_mapping_log_notification = 17_bit;
+		static inline constexpr alert_category_t communication_notification = 23_bit;
+		static inline constexpr alert_category_t communication_log_notification = 24_bit;
+		static inline constexpr alert_category_t blockchain_notification = 25_bit;
+		static inline constexpr alert_category_t blockchain_log_notification = 26_bit;
 		static inline constexpr alert_category_t all_categories = alert_category_t::all();
 
 		// hidden
